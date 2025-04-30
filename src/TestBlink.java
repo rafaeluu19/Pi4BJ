e6a51697c4c76b2233308fa158127af3b9ab74cb
Rafael <168372109+rafaeluu19@users.noreply.github.com>
2025-04-30 12:32:41 -0400

    Update and rename TestGPIO.java to TestBlink.java

---
+++ src/TestBlink.java
@@ -0,0 +1,40 @@
+import gpio.GPIOPin;
+import java.io.IOException;
+
+public class TestBlink 
+{
+    private GPIOPin pin; 
+    
+    public class TestBlink
+    {
+        try 
+        {
+            GPIOPin pin = new GPIOPin("17"); // BCM pin 17
+            pin.setDirection("out");
+        }
+        catch (IOException | InterruptedException e)
+        {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+
+    public void blinkLights()
+    {
+        try
+        {
+            System.out.println("Turning ON the LED...");
+            pin.setValue(true); // LED ON
+            Thread.sleep(1000); // Wait 1 second
+
+            System.out.println("Turning OFF the LED...");
+            pin.setValue(false); // LED OFF
+
+            pin.unexport(); // Clean up
+            System.out.println("GPIO pin unexported.");
+        }
+        catch (IOException | InterruptedException e) 
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }    
+}
